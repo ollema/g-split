c9cdefdddeaec44850d1c4edd5db474e71e22d4b
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-10-21 17:01:16 +0200

    object detection progress

---
+++ src/lib/components/ImageUpload.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { Snippet } from 'svelte';
+
+	let {
+		src = $bindable(),
+		img = $bindable(),
+		children
+	}: { src?: string; img?: HTMLImageElement; children: Snippet } = $props();
+
+	let dragging = $state(false);
+
+	let input: HTMLInputElement;
+
+	function handleDrag(e: DragEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+		dragging = e.type === 'dragenter' || e.type === 'dragover';
+	}
+
+	function handleDrop(e: DragEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+		dragging = false;
+		if (e.dataTransfer?.files && e.dataTransfer.files[0]) {
+			handleFiles(e.dataTransfer.files);
+		}
+	}
+
+	function handleChange(e: Event) {
+		e.preventDefault();
+		const target = e.target as HTMLInputElement;
+		if (target.files && target.files[0]) {
+			handleFiles(target.files);
+		}
+	}
+
+	function handleFiles(files: FileList) {
+		const file = files[0];
+		if (file.type.startsWith('image/')) {
+			src = URL.createObjectURL(file);
+		} else {
+			alert('Please upload an image file');
+		}
+	}
+
+	function onButtonClick() {
+		input.click();
+	}
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.key === 'Enter' || e.key === ' ') {
+			e.preventDefault();
+			onButtonClick();
+		}
+	}
+</script>
+
+<div class="flex w-full items-center justify-center">
+	<div
+		class={cn(
+			'flex w-full cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-gray-300 p-4 transition duration-300 ease-in-out hover:bg-gray-100',
+			dragging && 'border-blue-500 bg-blue-100'
+		)}
+		ondragenter={handleDrag}
+		ondragleave={handleDrag}
+		ondragover={handleDrag}
+		ondrop={handleDrop}
+		onclick={onButtonClick}
+		onkeydown={handleKeyDown}
+		role="button"
+		tabindex="0"
+	>
+		<input class="hidden" type="file" accept="image/*" onchange={handleChange} bind:this={input} />
+
+		<div class="w-fill relative flex aspect-square w-full max-w-lg items-center justify-center">
+			<div
+				class={cn('absolute flex flex-col items-center justify-center px-0 py-4', src && 'hidden')}
+			>
+				<p class="mb-2 text-sm text-gray-600">
+					<span class="font-semibold">Click to upload</span> or drag and drop
+				</p>
+				<p class="text-sm text-gray-600">Supported formats: SVG, PNG & JPG</p>
+			</div>
+
+			<img
+				class={cn('hidden object-contain object-center', src && 'block')}
+				{src}
+				alt="uploaded"
+				bind:this={img}
+			/>
+
+			{#if children}
+				{@render children()}
+			{/if}
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import BoundingBoxOverlay from '$lib/components/BoundingBoxOverlay.svelte';
+	import ImageUpload from '$lib/components/ImageUpload.svelte';
+	import { detect, type BoundingBox } from '$lib/models/object_detection/detect';
+
+	let src: string | undefined = $state(undefined);
+	let bboxes: BoundingBox[] = $state([]);
+	let latency: number[] | null = $state(null);
+
+	let detections = $derived.by(async () => {
+		if (!src) return undefined;
+		const result = await detect(src, 'cup');
+		if (result) {
+			bboxes = result.bboxes;
+			latency = result.latency;
+			return result;
+		}
+	});
+</script>
+
+<div class="mx-auto mt-6 flex max-w-md flex-col items-center gap-4">
+	<ImageUpload bind:src>
+		<BoundingBoxOverlay {bboxes} />
+	</ImageUpload>
+
+	{#if src}
+		{#await detections}
+			<p>Processing image...</p>
+		{:then}
+			{#if bboxes.length > 0 && latency !== null}
+				<div>
+					<p>Number of detections: {bboxes.length}</p>
+					<p>
+						Latency: {#each latency as l}
+							<span>{l.toFixed(3)}s</span>
+						{/each}
+					</p>
+				</div>
+			{/if}
+		{:catch error}
+			<p>{error.message}</p>
+		{/await}
+	{/if}
+</div>
