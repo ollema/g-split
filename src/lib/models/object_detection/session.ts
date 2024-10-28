a517d0d7d6a16f0395a42c70327c39be3d999613
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-10-28 21:57:34 +0100

    improve static asset handling

--- src/lib/models/object_detection/session.ts
+++ src/lib/models/object_detection/session.ts
@@ -1,19 +1,12 @@
 import * as ort from 'onnxruntime-web/webgpu';
+import wasm from '$lib/assets/wasm/ort-wasm-simd-threaded.jsep.wasm?url';
+import model from '$lib/assets/onnx/yolo11s.onnx?url';
 
-const DEFAULT_MODEL_FILE_PATH = '/yolo11s.onnx';
-const DEFAULT_EXECUTION_PROVIDERS = ['wasm', 'webgpu'];
-const DEFAULT_GRAPH_OPTIMIZATION_LEVEL = 'all';
+export async function createSession(): Promise<ort.InferenceSession> {
+	ort.env.wasm.wasmPaths = { wasm };
 
-export async function createSession(
-	model: string = DEFAULT_MODEL_FILE_PATH,
-	executionProviders = DEFAULT_EXECUTION_PROVIDERS,
-	graphOptimizationLevel = DEFAULT_GRAPH_OPTIMIZATION_LEVEL
-): Promise<ort.InferenceSession> {
-	ort.env.wasm.wasmPaths = '/';
-
-	// @ts-expect-error false positive
 	return ort.InferenceSession.create(model, {
-		executionProviders,
-		graphOptimizationLevel
+		executionProviders: ['wasm', 'webgpu'],
+		graphOptimizationLevel: 'all'
 	});
 }
